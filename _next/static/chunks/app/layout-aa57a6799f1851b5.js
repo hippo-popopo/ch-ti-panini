1d2d6c0c17427087b7aa30a08257854f2e617b45
Hippolyte De villeneuve <h2o@MacBook-Air-de-Hippolyte-3.local>
2026-09-23 14:04:35 +0200

    Publish static Au Ch’ti Panini website

---
+++ _next/static/chunks/app/layout-aa57a6799f1851b5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{480:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4748,23))},4748:()=>{}},e=>{e.O(0,[573,149,740,358],()=>e(e.s=480)),_N_E=e.O()}]);
---
+++ _next/static/chunks/next/dist/client/components/builtin/global-error-e00725e643bbd122.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[337],{1495:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,190,23))}},e=>{e.O(0,[149,740,358],()=>e(e.s=1495)),_N_E=e.O()}]);
